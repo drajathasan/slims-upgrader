0088fa3bc40f74978a80cf1e45036bf3a1750d60
Drajat Hasan <drajathasan20@gmail.com>
2022-11-21 16:28:32 +0700

    Update upgrade script

--- src/Upgrade.inc.php
+++ src/Upgrade.inc.php
@@ -1019,7 +1019,7 @@ ADD INDEX (  `input_date` ,  `last_update` ,  `uid` ) ;";
     function upgrade_role_31()
     {
         $sql['alter'][] = "ALTER TABLE `files` ADD `file_key` text COLLATE 'utf8_unicode_ci' NULL AFTER `file_desc`;";
-        if($_POST['oldVersion'] > 19) {
+        if($_POST['oldVersion']??0 > 19) {
           $sql['alter'][] = "ALTER TABLE `biblio` DROP `update_date`;";
         }
         $sql['alter'][] = "ALTER TABLE `mst_topic` CHANGE `classification` `classification` varchar(50) COLLATE 'utf8_unicode_ci' NULL COMMENT 'Classification Code' AFTER `auth_list`;";
